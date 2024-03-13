23564201f61c1985888186e9e4e1b79d1d703ae0
TurtIeSocks <TurtIeSocks@users.noreply.github.com>
2024-03-13 15:35:26 +0000

    deploy: 3343e7c7ce637c82bcc480ed11232b7a814beee4

--- example/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-
-import App from './App'
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(<App />)
